46addf39de568072ad5e0d1ec044a6e9fa5a805d
Luiz França <francaldr@gmail.com>
2019-10-01 22:17:02 -0300

    Task-1: Preventing adding todo without description

--- alayatodo/views.py
+++ alayatodo/views.py
@@ -65,11 +65,16 @@ def todos():
 def todos_POST():
     if not session.get('logged_in'):
         return redirect('/login')
-    g.db.execute(
-        "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
-        % (session['user']['id'], request.form.get('description', ''))
-    )
-    g.db.commit()
+
+    description = request.form.get('description').strip()
+
+    if description:
+        g.db.execute(
+            "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
+            % (session['user']['id'], description)
+        )
+        g.db.commit()
+
     return redirect('/todo')
 
 
